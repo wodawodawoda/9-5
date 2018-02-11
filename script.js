5aa4a1657d5c4325346a8fa8da9d4af8a7655f48
wodawodawoda <mateusz.wodnik@gmail.com>
2018-02-11 17:53:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // CREATE BUTTONS
 function createButtons(quantity, place) {
-	var place = document.getElementsByClassName(place);
-	for (i = 0; i < quantity; i++) {
-		var button = document.createElement('button');
+	place = document.getElementsByClassName(place);
+	for (let i = 0; i < quantity; i++) {
+		const button = document.createElement('button');
 		button.className = 'button';
 		button.innerHTML = 'button-' + i;
-		place[0].appendChild(button)
+		place[0].appendChild(button);
 	}
 }
 
 // DISPLAY BUTTONS INNER TEXT
-var allButtons = document.getElementsByClassName('button');
+const allButtons = document.getElementsByClassName('button');
 function btnInner() {
-	for (i = 0; i < allButtons.length; i++) {
-		var a = allButtons[i];
+	for (let i = 0; i < allButtons.length; i++) {
+		let a = allButtons[i];
 		console.log(a.innerText);
 	}
 }
+
+createButtons(10,'container');
+btnInner();
